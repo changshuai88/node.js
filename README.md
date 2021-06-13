d2865213250a7438e66e04b31e4756b3b5eba994
changshuai <2233282817@qq.com>
2021-06-13 23:06:09 +0800

    0613

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # node.js
+# HTTP
+HTTP(hypertext transport protocol)协议【超文本传输协议】，协议详细规定了浏览器和万维网服务器之间互相通信的规则。
+## 请求报文
+重点是格式与参数
+
+行      GET     /s?ie=utf-8     HTTP/1.1
+头      host: atguigu.com
+        cookie: name=guigu
+        content-type: application/x-www-form-urlencoded
+        user-Agent: chrome 83
+空行
+体      username=admin&password=admin
+
+## 响应报文
+
+行      HTTP/1.1  200   ok
+头      Content-Type：text/html;charset=utf-8
+        Content-length: 2048
+        content-encoding: gzip
+空行
+体      <html>
+            <head>
+                <body>
+                <h1>李世民</h1>
+                </body>
+            </head>
+        </html>
+
+# express框架
+
+
+# nodemon
+可以免除重复开关终端--使更改服务端代码时候变得方便
+这里如果安装不成功需要用管理员身份设置
+1.管理员身份打开powerShell
+
+2.输入set-ExecutionPolicy RemoteSigned 
---
+++ code/yuanshengAJAX/server.js
@@ -0,0 +1,49 @@
+//1.引入express
+const  express=require('express');
+//2.创建应用对象
+const app= express();
+
+//3.创建路由规则
+//requset 是对请求报文的封装
+//response 是对响应报文的封装
+app.get('/server',(request,response)=>{
+    //设置响应头  设置允许跨域
+    response.setHeader('Access-Control-Allow-Origin','*');
+    //设置响应体
+    response.send('HELLO AJAX-2');
+});
+
+// app.post('/server',(request,response)=>{
+//     //设置响应头  设置允许跨域
+//     response.setHeader('Access-Control-Allow-Origin','*');
+//     //允许自定义响应头--设置完还不行还要将post改为all，可以接收任意乐星请求
+//     response.setHeader('Access-Control-Allow-Headers','*');
+//     //设置响应体
+//     response.send('HELLO AJAX POST');
+// });
+//all可以接收任意类型的请求
+app.all('/json-server',(request,response)=>{
+    //设置响应头  设置允许跨域
+    response.setHeader('Access-Control-Allow-Origin','*');
+    //允许自定义响应头--设置完还不行还要将post改为all，可以接收任意乐星请求
+    response.setHeader('Access-Control-Allow-Headers','*');
+    //响应一个数据
+    const data={
+        name:'shuai'
+    };
+    //对对象进行字符串转换，借助JSON。stringify方法
+    let str=JSON.stringify(data);
+    //设置响应体  send方法只能接收字符串。
+    response.send(str);
+});
+//针对IE缓存
+app.get('/ie',(request,response)=>{
+    //设置响应头  设置允许跨域
+    response.setHeader('Access-Control-Allow-Origin','*');
+    //设置响应体
+    response.send('HELLO IE-3');
+});
+//4.鉴定端口启动服务
+app.listen(8000,()=>{
+    console.log('服务已经启动，8000 端口监听中......');
+});
