554c109080bb3af5b13ce7f84db13f08b740a32f
changshuai <2233282817@qq.com>
2021-06-15 21:46:40 +0800

    请求超时和网络异常

--- code/yuanshengAJAX/server.js
+++ code/yuanshengAJAX/server.js
@@ -43,6 +43,15 @@ app.get('/ie',(request,response)=>{
     //设置响应体
     response.send('HELLO IE-3');
 });
+//延时响应
+app.get('/delay',(request,response)=>{
+    //设置响应头  设置允许跨域
+    response.setHeader('Access-Control-Allow-Origin','*');
+    setTimeout(() => {
+        //设置响应体
+    response.send('延时响应');
+    }, 3000);
+});
 //4.鉴定端口启动服务
 app.listen(8000,()=>{
     console.log('服务已经启动，8000 端口监听中......');
